94f9d3ec03702d877287dce45ed3847fa1f4b06a
Rajnath Dutta <158247773+rajnathdutta123@users.noreply.github.com>
2024-12-12 16:11:58 +0530

    After Log4j2 Configuration

--- src/main/java/com/app/LaborNakuriBackend/controller/DummyController.java
+++ src/main/java/com/app/LaborNakuriBackend/controller/DummyController.java
@@ -1,5 +1,7 @@
 package com.app.LaborNakuriBackend.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DummyController {
 
+    private static final Logger logger = LogManager.getLogger(DummyController.class);
+
     @GetMapping("/hello")
-    public ResponseEntity<String> test()
-    {
+    public ResponseEntity<String> test() {
+        logger.info("Info level log example");
+        logger.debug("Debug level log example");
+        logger.error("Error level log example", new Exception("Example exception"));
         return ResponseEntity.ok("hello");
     }
 }
