7e46d28d1a33cf9e3073fa1defce87a30f6e6619
Rajnath Dutta <158247773+rajnathdutta123@users.noreply.github.com>
2024-12-13 15:45:45 +0530

    Custom Appender done

---
+++ src/main/java/com/app/LaborNakuriBackend/config/MapAppender.java
@@ -0,0 +1,39 @@
+package com.app.LaborNakuriBackend.config;
+
+import lombok.Data;
+import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Core;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+
+import java.time.Instant;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Plugin(name = "MapAppender", category = Core.CATEGORY_NAME, elementType = Appender.ELEMENT_TYPE, printObject = true)
+public class MapAppender extends AbstractAppender {
+
+
+    protected MapAppender(String name, Filter filter) {
+        super(name, filter, null);
+    }
+
+    @PluginFactory
+    public static MapAppender createAppender(
+            @PluginAttribute("name") String name,
+            @PluginElement("Filter") Filter filter) {
+        return new MapAppender(name, filter);
+    }
+
+    @Override
+    public void append(LogEvent event) {
+        System.out.println("Appending event: " + event.getMessage().getFormattedMessage());
+    }
+}
--- src/main/java/com/app/LaborNakuriBackend/controller/DummyController.java
+++ src/main/java/com/app/LaborNakuriBackend/controller/DummyController.java
@@ -1,10 +1,14 @@
 package com.app.LaborNakuriBackend.controller;
 
+import com.app.LaborNakuriBackend.config.MapAppender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LogEvent;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class DummyController {
@@ -15,7 +19,7 @@ public class DummyController {
     public ResponseEntity<String> test() {
         logger.info("Info level log example");
         logger.debug("Debug level log example");
-        logger.error("Error level log example", new Exception("Example exception"));
+        logger.error("Error level log example");
         return ResponseEntity.ok("hello");
     }
 }
